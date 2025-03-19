9f0c77860fabc0f0511dd2f0130a1f890b2756d4
sanp1217 <pablosgrad12@gmail.com>
2025-03-19 17:57:15 -0600

    initial files

---
+++ HW6/wytar.c
@@ -0,0 +1,11 @@
+/*
+wyls.c
+Author: Pablo Sanchez
+Date: Feb 22, 2025
+
+COSC 3750, Homework 5
+
+a version of ls that only allows the -h or -n arguments.
+-h means "human readable" output. -n means replace username
+and group name with uid and gid.  
+*/
